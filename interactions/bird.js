6c36c9e59400206c7698c5bec8ba31b24260992c
Smile <85846415+Smile249@users.noreply.github.com>
2022-11-19 10:52:17 +0100

    v5
    colors added for cat command, bird command added

--- interactions/bird.js
+++ interactions/bird.js
@@ -11,23 +11,23 @@ const {
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("cat")
-    .setDescription("Sends a picture of a cat.")
+    .setName("bird")
+    .setDescription("Sends a picture of a bird.")
     .setDMPermission(true),
   async execute(interaction) {
     await interaction.deferReply();
 
-    fetch("https://some-random-api.ml/animal/cat")
+    fetch("https://some-random-api.ml/animal/bird")
       .then((res) => res.json())
       .then((data) => {
-        let catEmbed = new EmbedBuilder()
+        let birdEmbed = new EmbedBuilder()
         .setFooter({
             text: data.fact,
-            iconURL: "https://www.google.com/logos/fnbx/animal_paws/cat_kp_dm.gif"
+            iconURL: "https://lh3.googleusercontent.com/u/0/drive-viewer/AFDK6gPDH2zMpL4i4JCbRr9mEZqdgzarMFtUd3URZmTc8MXlCdxP5o_AaAKf42Pc0cLV-PKruMRAB2DyAW4vXGC_WdxGVVTZbA=w1488-h927"
         })
         .setImage(data.image)
-        .setColor("#d4742c")
-        return interaction.editReply({ embeds: [catEmbed] });
+        .setColor("#2C5F23")
+        return interaction.editReply({ embeds: [birdEmbed] });
       });
   },
 };
